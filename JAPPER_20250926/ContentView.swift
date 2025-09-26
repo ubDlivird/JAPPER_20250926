44cf1420729cbd8b07f8a6f2ab5ca49983fb6877
SHUYA <articulation_kurenai@yahoo.co.jp>
2025-09-26 11:09:56 +0900

    Initial Commit

---
+++ JAPPER_20250926/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JAPPER_20250926
+//
+//  Created by SHUYA on 2025/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JAPPER_20250926/JAPPER_20250926App.swift
@@ -0,0 +1,17 @@
+//
+//  JAPPER_20250926App.swift
+//  JAPPER_20250926
+//
+//  Created by SHUYA on 2025/09/26.
+//
+
+import SwiftUI
+
+@main
+struct JAPPER_20250926App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
